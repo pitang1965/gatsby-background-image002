4135001fb37bccad8b0cea256f1853b4b80cea54
Shoji Makino <pitang1965@gmail.com>
2021-03-23 21:17:10 +0900

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,55 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import BackgroundImage from 'gatsby-background-image';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const StyledBackground = styled(BackgroundImage)`
+  width: 100%;
+  height: 60vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
 
-const IndexPage = () => (
+const BlackOverlay = styled.div`
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.3);
+  height: 100%;
+  align-items: center;
+  color: white;
+`;
+
+const ContentBox = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const IndexPage = props => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <StyledBackground fluid={props.data.indexImage.childImageSharp.fluid}>
+      <BlackOverlay>
+        <ContentBox>
+          <h1>London</h1>
+          <h2>27 Apr, 2004</h2>
+        </ContentBox>
+      </BlackOverlay>
+    </StyledBackground>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    indexImage: file(relativePath: { eq: "london.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 2560) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
