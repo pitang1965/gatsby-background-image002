5a86a4a53e18324d4c05f7947d02072fac06f675
Shoji Makino <pitang1965@gmail.com>
2021-03-24 12:08:32 +0900

    Change title to 002

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Background Image 001`,
+    title: `Gatsby Background Image 002`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
